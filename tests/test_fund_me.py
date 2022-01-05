2b5a36bcc56b993957addf732c3bd234df107397
Lenovo <benasvol@gmail.com>
2022-01-05 21:29:43 +0200

    Write test

--- tests/test_fund_me.py
+++ tests/test_fund_me.py
@@ -1,22 +1,38 @@
 from scripts.helpful_scripts import get_account, LOCAL_BLOCKCHAIN_ENVS
 from scripts.deploy import deploy_fund_me
-from brownie import network
+from brownie import network, accounts, exceptions
 import pytest
 
-
+# Test if whole fund and withdraw process works
 def test_can_fund_and_withdraw():
+    # Arrange
     account = get_account()
     fundMe = deploy_fund_me()
     entranceFee = fundMe.getEntranceFee()
+
+    # Act 1
     tx = fundMe.fund({"from": account, "value": entranceFee})
     tx.wait(1)
+    # Assert 1
     assert fundMe.addressToAmountFunded(account.address) == entranceFee
 
+    # Act 2
     tx2 = fundMe.withdraw({"from": account})
     tx2.wait(1)
+    # Assert 2
     assert fundMe.addressToAmountFunded(account.address) == 0
 
 
 def test_only_owner_can_withdraw():
+    # Make this test run only on local blockchains, because running it on mainnet can take a long time
     if network.show_active() not in LOCAL_BLOCKCHAIN_ENVS:
         pytest.skip("only for local testing")
+
+    # Arrange
+    fundMe = deploy_fund_me()
+    fakeAccount = accounts.add()
+
+    # Act
+    fundMe.withdraw({"from": fakeAccount})
+
+    # Assert
